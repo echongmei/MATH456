d2bdfd66f58b9f711118d4f9a1ae4ac7e2d75dd7
Savannah-Arimento <savannaharimento@gmail.com>
2023-05-01 12:21:05 -0400

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,29 @@
 from strategies import *
-'hi'
+import random 
+
+
+def generate_players(N : int):
+    players = dict()
+    for i in range(N-1):
+        strategy_val = random.randint(1, 6) 
+        if strategy_val == 1:
+        
+        if strategy_val == 2:
+            
+        if strategy_val == 3:
+            
+        if strategy_val == 4:
+            
+        if strategy_val == 5:
+            
+        if strategy_val == 6:
+            
+        
+        
+    
+    
 def neural_network():
     return
+
+
+generate_players(2)
