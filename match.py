b9ac1f5ad45edc2aaf69aee05c38d378617a78c7
Elisa Chong <60830442+echongmei@users.noreply.github.com>
2023-05-04 15:33:17 -0400

    Update match.py

--- match.py
+++ match.py
@@ -1,5 +1,5 @@
 class Match():
-    """condicts matches between two players"""
+    """conducts matches between two players"""
     
     def __init__(self, players = None, turns=None):
         """ 
@@ -14,18 +14,27 @@ class Match():
         
         self.players = players
         self.turns = turns
-
-    def play(self):
-        """
-        Plays one round of the match
-        """
-        return
+        self.payoff_matrix = [[[-1,-1], [-6,0]], [[0,-6],[-4,-4]]]
+        
+    # def play(self):
+    #     """
+    #     Plays one round of the match
+    #     """
+    #     return
     
-    def winner(self):
+    def winner(self, players):
         """
         Returns the winner of the match
         """
-        return
+        max_score = -10000
+        winner_player_id = -1
+        for p in players:
+            if max_score < p.total_score:
+                max_score = p.total_score
+                winner_player_id = p.player_id
+        if winner_player_id == -1:
+            raise ValueError("Failed to find winner")
+        return winner_player_id
     
     def scores(self):
         """
